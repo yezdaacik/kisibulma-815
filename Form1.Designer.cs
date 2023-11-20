fa6beb221d272aafc86182c72de18a9eb5b493b6
Yezda <145451081+yezdanuracik@users.noreply.github.com>
2023-11-20 11:56:00 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace arama_ornegi_815
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbKisi = new System.Windows.Forms.ListBox();
+            this.btnAra = new System.Windows.Forms.Button();
+            this.txtAra = new System.Windows.Forms.TextBox();
+            this.lblSonuc = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbKisi
+            // 
+            this.lbKisi.FormattingEnabled = true;
+            this.lbKisi.Items.AddRange(new object[] {
+            "Yezda ",
+            "Nursevim",
+            "Hilal ",
+            "Ravza",
+            "Yezda ",
+            "Beşiktaş",
+            "Nursevim",
+            "Ravza",
+            "Ecrin",
+            "Yezda ",
+            "Hilal ",
+            "Messi",
+            "Beşiktaş"});
+            this.lbKisi.Location = new System.Drawing.Point(23, 83);
+            this.lbKisi.Name = "lbKisi";
+            this.lbKisi.Size = new System.Drawing.Size(181, 238);
+            this.lbKisi.TabIndex = 0;
+            // 
+            // btnAra
+            // 
+            this.btnAra.Location = new System.Drawing.Point(129, 37);
+            this.btnAra.Name = "btnAra";
+            this.btnAra.Size = new System.Drawing.Size(75, 23);
+            this.btnAra.TabIndex = 1;
+            this.btnAra.Text = "Ara";
+            this.btnAra.UseVisualStyleBackColor = true;
+            this.btnAra.Click += new System.EventHandler(this.btnAra_Click);
+            // 
+            // txtAra
+            // 
+            this.txtAra.Location = new System.Drawing.Point(23, 40);
+            this.txtAra.Name = "txtAra";
+            this.txtAra.Size = new System.Drawing.Size(100, 20);
+            this.txtAra.TabIndex = 2;
+            // 
+            // lblSonuc
+            // 
+            this.lblSonuc.AutoSize = true;
+            this.lblSonuc.Location = new System.Drawing.Point(20, 341);
+            this.lblSonuc.Name = "lblSonuc";
+            this.lblSonuc.Size = new System.Drawing.Size(58, 13);
+            this.lblSonuc.TabIndex = 3;
+            this.lblSonuc.Text = "..... kişi var";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(291, 410);
+            this.Controls.Add(this.lblSonuc);
+            this.Controls.Add(this.txtAra);
+            this.Controls.Add(this.btnAra);
+            this.Controls.Add(this.lbKisi);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbKisi;
+        private System.Windows.Forms.Button btnAra;
+        private System.Windows.Forms.TextBox txtAra;
+        private System.Windows.Forms.Label lblSonuc;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace arama_ornegi_815
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAra_Click(object sender, EventArgs e)
+        {
+            string aranan = txtAra.Text;
+            bool bulundu = false;
+            int sayac = 0;
+
+            for (int i =0; i < 20; i++)
+            {
+                string kisi = Convert.ToString(lbKisi.Items[i]);
+                if (kisi == lbKisi.Items[i].ToString()) 
+                {
+                    bulundu = true;
+                    sayac++;
+                }
+            }
+            if (bulundu)
+            {
+                lblSonuc.Text = "Aranan kişiden" + sayac+ "tane var";
+            }
+            else
+            {
+                lblSonuc.Text = "Aranan kişi bulunmadı";
+            }
+        }
+    }
+}
